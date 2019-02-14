d8da199f4985883ac4c1a429dbd69ed38239af86
Gabriel Derrien <gderrien@etu.uqac.ca>
2019-02-14 09:52:15 -0500

    Added ex2 travelling cam and initiated ex4

---
+++ Assets/Exercices/2/FOVTextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FOVTextManager : MonoBehaviour
+{
+    public static float FOV;
+    Text text;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        FOV = TravellingCameraController.angle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "FOV : " + FOV;
+    }
+}
